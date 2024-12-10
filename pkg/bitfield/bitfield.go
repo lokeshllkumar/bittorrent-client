49b82c405500c3be65c2e217e6b2c713ea3c6d84
lokeshllkumar <lokeshkumar2015.2004@gmail.com>
2024-12-11 00:52:42 +0530

    implemented Bitfield manipulation logic

---
+++ pkg/bitfield/bitfield.go
@@ -0,0 +1,28 @@
+package bitfield
+
+import (
+
+)
+
+type Bitfield []byte
+
+func (bf Bitfield) HasPiece(ind int) bool {
+	byteInd := ind / 8
+	offset := ind % 8
+	if byteInd < 0 || byteInd >= len(bf) {
+		return false
+	}
+
+	return bf[byteInd] >> uint(7 - offset) & 1 != 0
+}
+
+func (bf Bitfield) SetPiece(ind int) {
+	byteInd := ind / 8
+	offset := ind % 8
+
+	if byteInd < 0 || byteInd >= len(bf) {
+		return
+	}
+
+	bf[byteInd] |= 1 << uint(7 - offset)
+}
